6168a1610e75f2b3c91dcc55391a7fd95e9f6d76
Himanshu Ranjan <h4himanshu.ranjan@gmail.com>
2019-08-01 02:43:35 +0530

    linting of pattern.py file is done

--- cmdlaunch/programs/print_pattern/pattern.py
+++ cmdlaunch/programs/print_pattern/pattern.py
@@ -1,20 +1,21 @@
 import turtle
 import math
 import colorsys
-     
+
 phi = 180 * (3 - math.sqrt(5))
-    
+
 t = turtle.Pen()
 t.speed(0)
-    
+
+
 def square(t, size):
     for tmp in range(0,4):
         t.forward(size)
         t.right(90)
-    
-    
+
+
 num = 200
-    
+
 for x in reversed(range(0, num)):
     t.fillcolor(colorsys.hsv_to_rgb(x/num, 1.0, 1.0))
     t.begin_fill()
@@ -23,5 +24,5 @@ for x in reversed(range(0, num)):
     t.end_fill()
     t.right(phi)
     t.right(.8)
-    
+
 turtle.mainloop()
